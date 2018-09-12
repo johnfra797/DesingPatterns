cd22fa90410f422b98fc2db3b7fd456f269df99b
unknown <johnfra797@hotmail.com>
2018-09-11 22:15:01 -0500

    DAO T

---
+++ Desing-Patterns/NET/PatternsDesign/Structural/DAO/ICRUD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsDesign.Structural.DAO
+{
+    public interface ICRUD<T>
+    {
+
+        List<T> ObtenerTodos();
+        void actualizar(T t);
+        void borrar(T t);
+        void Agregar(T t);
+
+    }
+}
